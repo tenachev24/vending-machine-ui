e17d46429e92276443528ab7150757b9058bff2b
teodor <t.nachev@vconnect.dk>
2022-07-19 15:41:28 +0300

    - markup

--- src/components/VendingMachineSkeleton.vue
+++ src/components/VendingMachineSkeleton.vue
@@ -4,217 +4,199 @@
     <div class="vend-container">
       <div class="vend-outer">
         <div class="vend-back">
-          <div class="product-row">
-            <div class="tag tag-one">
-              <p>A1</p>
-            </div>
-            <div class="tag tag-two">
-              <p>A2</p>
-            </div>
-            <div class="tag tag-three">
-              <p>A3</p>
-            </div>
-            <div class="tag tag-four">
-              <p>A4</p>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+          <div class="container">
+            <div class="product-row">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-one">
+                      <p>A1</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-two">
+                      <p>A2</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-three">
+                      <p>A3</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-four">
+                      <p>A4</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+            <div class="product-row">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-one">
+                      <p>B1</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-two">
+                      <p>B2</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-three">
+                      <p>B3</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-four">
+                      <p>B4</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+            <div class="product-row">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-one">
+                      <p>C1</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-two">
+                      <p>C2</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-three">
+                      <p>C3</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-four">
+                      <p>C4</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+            <div class="product-row">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-one">
+                      <p>D1</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-two">
+                      <p>D2</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-three">
+                      <p>D3</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="spiral">
+                <div class="inner-spiral">
+                  <div class="inner-spiral">
+                    <div class="tag tag-four">
+                      <p>D4</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="product-row">
-            <div class="tag tag-one">
-              <p>B1</p>
-            </div>
-            <div class="tag tag-two">
-              <p>B2</p>
-            </div>
-            <div class="tag tag-three">
-              <p>B3</p>
-            </div>
-            <div class="tag tag-four">
-              <p>B4</p>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+            <div class="product-row">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-one">
+                      <p>E1</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-two">
+                      <p>E2</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+              <div class="spiral">
                 <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-three">
+                      <p>E3</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
+              <div class="spiral">
                 <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="product-row">
-            <div class="tag tag-one">
-              <p>C1</p>
-            </div>
-            <div class="tag tag-two">
-              <p>C2</p>
-            </div>
-            <div class="tag tag-three">
-              <p>C3</p>
-            </div>
-            <div class="tag tag-four">
-              <p>C4</p>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="product-row">
-            <div class="tag tag-one">
-              <p>D1</p>
-            </div>
-            <div class="tag tag-two">
-              <p>D2</p>
-            </div>
-            <div class="tag tag-three">
-              <p>D3</p>
-            </div>
-            <div class="tag tag-four">
-              <p>D4</p>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="product-row">
-            <div class="tag tag-one">
-              <p>E1</p>
-            </div>
-            <div class="tag tag-two">
-              <p>E2</p>
-            </div>
-            <div class="tag tag-three">
-              <p>E3</p>
-            </div>
-            <div class="tag tag-four">
-              <p>E4</p>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
-                </div>
-              </div>
-            </div>
-            <div class="spiral">
-              <div class="inner-spiral">
-                <div class="inner-spiral">
-
+                  <div class="inner-spiral">
+                    <div class="tag tag-four">
+                      <p>E4</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="vend-glass" />
+          <div class="vend-glass"/>
         </div>
       </div>
     </div>
